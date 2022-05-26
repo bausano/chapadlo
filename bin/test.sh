0518a4e041c3921fb9bfb6104c1a535220a65fe7
porkbrain <git@porkbrain.com>
2022-05-26 19:43:35 +0200

    Some docs for the test file

--- bin/test.sh
+++ bin/test.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+#
+# Runs unit tests and invokes the binary with a test file.
+#
+# We sort the lines of the output because the output is not deterministic in
+# order of client ids. That the header is always the first line is asserted
+# in unit tests.
+
 cargo test
 
 test_file_1_output="$(cargo run -- test/assets/input1.csv | sort)"
