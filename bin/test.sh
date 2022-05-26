7d82864bd63247e557aaefc65e5270772406dc90
porkbrain <git@porkbrain.com>
2022-05-26 19:41:49 +0200

    Asserting that the output is sort of what we expect

--- bin/test.sh
+++ bin/test.sh
@@ -2,4 +2,13 @@
 
 cargo test
 
-cargo run -- test/assets/input1.csv
+test_file_1_output="$(cargo run -- test/assets/input1.csv | sort)"
+expected_test_file_1_output="1,0.5000,3.0,3.5000,false
+2,3.0,0.0,3.0,true
+client,available,held,total,locked"
+
+echo
+echo
+[[ "${expected_test_file_1_output}" == "${test_file_1_output}" ]] \
+    && echo "✔ Test 1 one passed" \
+    || echo "✘ Test 1 failed"
